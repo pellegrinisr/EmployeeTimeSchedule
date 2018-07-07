b3fc928178cf59740a6a6913f90c1d7e228038f9
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-07 10:39:35 -0700

    beginnings of js file

---
+++ employeeTime.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC3U1A-0ntSJRmVvPMH3jY6fIK8Iy4eEG8",
+        authDomain: "timesheet-eb833.firebaseapp.com",
+        databaseURL: "https://timesheet-eb833.firebaseio.com",
+        projectId: "timesheet-eb833",
+        storageBucket: "timesheet-eb833.appspot.com",
+        messagingSenderId: "783877785753"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+})
