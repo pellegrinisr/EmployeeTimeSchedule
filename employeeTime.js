becaf6bda3097bee9e2018bd3bcac67c3b1bdbca
Stephen Pellegrini <spellegrini156@gmail.com>
2018-07-07 11:50:03 -0700

    most recent js

--- employeeTime.js
+++ employeeTime.js
@@ -32,6 +32,9 @@ $(document).ready(function() {
             startDate: startDate,
             monthlyRate: monthlyRate
         });
-
+        $('#fname').val('');
+        $('#lname').val('');
+        $('#startDate').val('');
+        $('#monthlyRate').val('');
     });
 })
